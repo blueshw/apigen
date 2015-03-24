af8b406a9776b2c94b7cfb01d26eb077906e4e8b
wapj <wapj2000@gmail.com>
2015-03-24 17:04:03 +0900

    utf-8문제 수정

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,15 @@
+#-*- coding : utf-8 -*-
+from __future__ import unicode_literals
 from django.contrib import admin
 from pages.models import Project , Group, Api
+from import_export.admin import ImportExportModelAdmin
 
 admin.site.register(Project)
 admin.site.register(Group)
-admin.site.register(Api)
 
+class ApiAdmin(ImportExportModelAdmin):
+    list_display = ('__unicode__', 'group', 'name', 'method', 'url', 'param', 'desc', 'request', 'response')
+
+
+#admin.site.register(Api, ApiAdmin)
+admin.site.register(Api)
--- pages/models.py
+++ pages/models.py
@@ -1,36 +1,28 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
-import string
 
 class Project(models.Model):
 	user = models.ForeignKey(User)
 	title = models.CharField(max_length=200)
 	create_dt = models.DateTimeField('date created')
 
-	def __unicode__ (self):
-		return self.title
-
 class Group(models.Model):
 	project = models.ForeignKey(Project)
 	title = models.CharField(max_length=200)
 	create_dt = models.DateTimeField('date created')
 
-	def __unicode__ (self):
-		return self.title
-
 class Api(models.Model):
 	group = models.ForeignKey(Group)
-	name = models.CharField(max_length=100)
-	method = models.CharField(max_length=10)
-	url = models.CharField(max_length=200)
+	name = models.CharField("이름", max_length=100)
+	method = models.CharField("메서드", max_length=10)
+	url = models.CharField("유알엘",max_length=200)
 	param = models.CharField(max_length=256)
-	desc = models.TextField('description')
+	desc = models.TextField("상세설명",'description')
 	request= models.TextField('request')
 	response = models.TextField('response')
 	create_dt = models.DateTimeField('date created')
-	
-	def __unicode__ (self):
-		return self.name
 	
 	def html_desc(self):
 		return self.desc.replace('\r\n', '<br />')
